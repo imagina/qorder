33727823cb47aa760d819467ce1ce57a159008f5
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-12-18 18:41:44 -0500

    + update translation

--- _i18n/en-us/index.js
+++ _i18n/en-us/index.js
@@ -9,26 +9,28 @@ export default {
       "label": {
         "refuse": "Reject",
         "refuseOrder": "Reject Order",
-        "refuseMessage": "Do you want to permanently reject this order? Once rejected, there is no turning back. Please make sure.",
+        "refuseMessage": "Do you want to permanently reject this order? Once rejected, there is no going back. Please make sure.",
         "accept": "Accept",
         "acceptOrder": "Accept Order",
         "acceptMessage": "Do you want to accept this order? Please ensure the units and price are correct.",
-        "suppliesHelp": "This module allows you to manage orders: review, edit quantities or prices if necessary, and decide whether to accept or reject them. Be sure to check the details before confirming."
+        "suppliesHelp": "This module allows you to manage orders: review, edit quantities or prices if needed, and decide whether to accept or reject them. Make sure to verify the details before confirming."
       },
       "form": {
         "order": "Order",
         "orderId": "# Order",
         "supplier": "Supplier",
+        "suppliers": "Suppliers",
         "price": "Price",
         "supplierPrice": "Supplier Price",
         "requestedQuantity": "Requested Units",
         "avaliableQuantity": "Available Units",
         "requestedAndAvaliableQuantity": "Requested/Available Units",
         "observations": "Observations",
-        "beforeUpdate" : {
+        "total": "Total",
+        "beforeUpdate": {
           "invalidPrice": "Enter a valid price.",
           "invalidQuantity": "Enter a valid quantity.",
-          "invalidAvaliableQuantity": "The available units must be less than the requested ones."
+          "invalidAvaliableQuantity": "The number of available units must be less than the requested units."
         }
       },
       "orderManagement": "Orders",
